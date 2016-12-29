651dbcf0b877b34fb7ea50aa73ed7d7e546a9c21
jackjava003 <jackjava003@gmail.com>
2016-12-29 16:46:34 +0800

    Example update

---
+++ JMS_Self_Prac/src/main/java/example/_01MessageSendAndReceive.java
@@ -0,0 +1,42 @@
+package example;
+
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue; 
+
+public class _01MessageSendAndReceive {
+
+	public static void main(String[] args) throws JMSException {
+		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("vm://localhost");
+		Connection connection = factory.createConnection();
+		connection.start();
+		Queue queue = new ActiveMQQueue("testQueue");
+		final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Scanner sc = new Scanner(System.in);
+		MessageProducer producer = session.createProducer(queue);
+		MessageConsumer comsumer = session.createConsumer(queue);
+		while(true){
+			
+			if(sc.hasNext()){
+				Message message = session.createTextMessage(sc.nextLine());
+				producer.send(message);
+				System.out.println("Send Message Completed!");
+			}
+			Message recvMessage = comsumer.receive();
+			System.out.println("Message Recieved: "+((TextMessage) recvMessage).getText());
+			recvMessage.clearBody();
+		}
+	}
+
+}
